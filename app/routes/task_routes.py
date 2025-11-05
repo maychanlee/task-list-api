05ac704f053ccd1235a932ef75aac28387f2a387
May Lee <mayhclee@gmail.com>
2025-11-04 23:54:23 -0800

    finished wave 1: CRUD for Task Model and added response body to test

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from ..db import db
+from .route_utilities import validate_model, create_model, get_models_with_filters
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+#create a task
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+    return create_model(Task, request_body)
+
+@bp.get("")
+def get_all_tasks():
+    return get_models_with_filters(Task, request.args)
+
+#get tasks: getting saved tasks
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return task.to_dict()
+
+@bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+@bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
